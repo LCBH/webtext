5c9958ba8ac6cbcfcddb396ecfe053012c1a6ce6
Lucca Hirschi <lucca.hirschi@gmail.com>
2014-10-25 23:22:40 +0200

    todo on rasp + rm request' stuffs

--- src/rasp/bashReceive.sh
+++ src/rasp/bashReceive.sh
@@ -3,9 +3,13 @@
 # It forwards received messages to PROGRAM, given him the senders' number
 # and the SMS' content.
 echo Starting screen of bashReceive:
-PROGRAM=/home/lutcheti/webtext/src/handleSMS.py # TODO: config auto
+PROGRAM=/home/lutcheti/webtext/src/request/handleSMS.py # TODO: config auto
 for i in `seq $SMS_MESSAGES` ; do
     echo "screen -S parseAndSend -d -m $PROGRAM \"\${SMS_${i}_NUMBER}\" \"\${SMS_${i}_TEXT}\" &"
     eval "screen -S parseAndSend -d -m $PROGRAM \"\${SMS_${i}_NUMBER}\" \"\${SMS_${i}_TEXT}\" &"
 done
 echo Ending screen of bashReceive.
+
+# TODO: From now, on: raspberry sends (number,SMS) to the "request server"
+# replace here the call of handleSMS.py by a wget request
+# to it using Python and log everything locally
