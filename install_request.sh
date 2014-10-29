223412336996b30cadf2899ef88769e93d45e487
Lucca Hirschi <lucca.hirschi@gmail.com>
2014-10-29 10:52:12 +0100

    Revert "added the *%?! dependency in php in the install_request script"
    This reverts commit 2356dc5da4458f78f284050493d1864ab7d21417.

--- install_request.sh
+++ install_request.sh
@@ -1,4 +1,3 @@
 # to do before using the "request server"
-sudo aptitude install python-html2text python-mechanize libyaml-0-2 python2.7-simplejson python-setuptools pyqt4-dev-tools python-dev libxml2-dev libxslt1-dev python-prettytable zlib1g-dev 
+sudo aptitude install python-html2text python-mechanize libyaml-0-2 python2.7-simplejson python-setuptools pyqt4-dev-tools python-dev libxml2-dev libxslt1-dev python-prettytable zlib1g-dev
 sudo aptitude install weboob
-sudo aptitude install php5 
