5df01a435572a0d814a070a7f8298ede717c49b3
Lucca Hirschi <lucca.hirschi@gmail.com>
2014-11-12 16:13:11 +0100

    + send 1 SMS per month (avoid to get the line closed)

--- install_rasp.sh
+++ install_rasp.sh
@@ -7,3 +7,6 @@ mkdir data/SMS/error
 mkdir data/SMS/sent
 mkdir data/SMS/outbox
 chown gammu -R data
+
+# ADD ton crontab (send 1 SMS per month to avoid to get the line closed):
+# 0 0 1 * * §home/lutcheti/webtext/src/rasp/send_sms.sh
