ef9819c6f5a73085081080fabde81264226e7cbd
Lucca Hirschi <lucca.hirschi@gmail.com>
2014-11-14 19:07:24 +0100

    no more relative path

--- src/rasp/bashReceive.sh
+++ src/rasp/bashReceive.sh
@@ -5,7 +5,7 @@
 # to the request server as well.
 echo Starting screen of bashReceive:
 PROGRAM=/home/lutcheti/webtext/src/request/handleSMS.py # TODO: config auto
-source ../../config_rasp.cfg
+source /home/lutcheti/webtext/config_rasp.cfg
 for i in `seq $SMS_MESSAGES` ; do
     echo "screen -S parseAndSend -d -m python $PROGRAM \"\${SMS_${i}_NUMBER}\" \"\${SMS_${i}_TEXT}\" \"false\" \"true\" &"
     eval "screen -S parseAndSend -d -m python $PROGRAM \"\${SMS_${i}_NUMBER}\" \"\${SMS_${i}_TEXT}\" \"false\" \"true\" &"
