54d92ca41ac952428aa08e965e495cb788fc0efa
Lucca Hirschi <lucca.hirschi@gmail.com>
2014-11-14 16:09:50 +0100

    secret key of API is now private

--- src/rasp/bashReceive.sh
+++ src/rasp/bashReceive.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # This program is executed any time a new SMS is received.
-# It forwards received messages to PROGRAM, given him the senders' number
-# and the SMS' content.
+# It forwards received messages to handleSMS (for requests that needs private data)
+# given him the senders' number and the SMS' content AND sends the request through HTTP
+# to the request server as well.
 echo Starting screen of bashReceive:
 PROGRAM=/home/lutcheti/webtext/src/request/handleSMS.py # TODO: config auto
-API_KEY=bhk126T74IY5sdfBNdfg35
+source ../../config_rasp.cfg
 for i in `seq $SMS_MESSAGES` ; do
     echo "screen -S parseAndSend -d -m python $PROGRAM \"\${SMS_${i}_NUMBER}\" \"\${SMS_${i}_TEXT}\" \"false\" \"true\" &"
     eval "screen -S parseAndSend -d -m python $PROGRAM \"\${SMS_${i}_NUMBER}\" \"\${SMS_${i}_TEXT}\" \"false\" \"true\" &"
@@ -12,7 +13,3 @@ for i in `seq $SMS_MESSAGES` ; do
     eval "curl -X 'https://www.choum.net/webtext/api/sms.php?pass=$API_KEY&numero=\${SMS_${i}_NUMBER}&content=\${SMS_${i}_TEXT}\'"
 done
 echo Ending screen of bashReceive.
-
-# TODO: From now, on: raspberry sends (number,SMS) to the "request server"
-# replace here the call of handleSMS.py by a wget request
-# to it using Python and log everything locally
--- src/rasp/send_sms.sh
+++ src/rasp/send_sms.sh
@@ -1,2 +1,4 @@
+# This script sends a SMS from the RaspBerry Pi's SIM card
+# to be executed once a month
 cd /home/lutcheti/webtext/data/SMS/outbox
 sudo gammu-smsd-inject -c /etc/gammu-smsdrc TEXT 0632368091 -text "Coucou c'est la RaspBerry Pi. Je t'envoie mon SMS mensuel."
