b7375273ad34c59cb88376346c6fccc9335d926d
Lucca Hirschi <lucca.hirschi@gmail.com>
2014-11-17 15:03:14 +0100

    set up shortcuts (personnal shortcuts for list of requests)

--- src/request/test.py
+++ src/request/test.py
@@ -54,5 +54,6 @@ callHandle("Coucou", user1['number'])
 callHandle("cine mommy 75018", user2['number'])
 callHandle("velo marx dormoy", user1['number'])
 callHandle("meteo 75020", user1['number'])
+callHandle("retour", user1['number'])
 
 # TODO: focus on testing all backends
