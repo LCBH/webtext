3411a6393e47a8b6e7173ac4c5f5f8d073b5d744
Lucca Hirschi <lucca.hirschi@gmail.com>
2015-05-01 15:11:38 +0200

    + utils.py containing common helping functions

--- src/request/parse.py
+++ src/request/parse.py
@@ -34,7 +34,9 @@ import unicodedata
 
 import fetch
 import database.db
+
 from static import *
+from utils import *
 from backends.main import Backend
 from request import Request
 
@@ -63,12 +65,6 @@ logging = logging.getLogger(__name__)
 
 # Global dictionnary containing options (if there is more than 1 request, options of later requests override previous ones)
 optionsDict = {}
-
-def simplifyText(s):
-    """Remove extra blanks, replace uppercase letters by lowercase letters and remove all accents from 's'."""
-    s1 = unicode(s.strip().lower(), 'utf-8')
-    s2 = unicodedata.normalize('NFD', s1).encode('ascii', 'ignore')     
-    return(s2)
 
 def parseRequest(request, user, is_local, is_testing, config_backends):
     """ Parse a request (i.e., instance of class Request) and returns the expected answer. """
---
+++ src/request/utils.py
@@ -0,0 +1,43 @@
+# -- coding: utf-8 --
+###########################################################################
+#                                                                         #
+#                       WebText                                           #
+#                                                                         #
+#                       Lucca Hirschi                                     #
+#                       <lucca.hirschi@ens-lyon.fr>                       #
+#                                                                         #
+#    Copyright 2014 Lucca Hirschi                                         #
+#                                                                         #
+#    This file is part of OwnShare.                                       #
+#    OwnShare is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by #
+#    the Free Software Foundation, either version 3 of the License, or    #
+#    (at your option) any later version.                                  #
+#                                                                         #
+#    OwnShare is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+#    GNU General Public License for more details.                         #
+#                                                                         #
+#    You should have received a copy of the GNU General Public License    #
+#    along with OwnShare.  If not, see <http://www.gnu.org/licenses/>.    #
+#                                                                         #
+###########################################################################
+
+""" Helping functions. """
+
+from __future__ import unicode_literals # implicitly declaring all strings as unicode strings
+BLANK = " "
+
+def simplifyText(s):
+    """Remove extra blanks (outside), replace uppercase letters by lowercase letters and remove all accents from 's'."""
+    s1 = unicode(s.strip().lower(), 'utf-8')
+    s2 = unicodedata.normalize('NFD', s1).encode('ascii', 'ignore')     
+    return(s2)
+
+def compactText(s):
+    """ Remove extra blanks (at most one blank). """
+    while (BLANK + BLANK) in s:
+        print(s)
+        s = s.replace(BLANK + BLANK, BLANK)
+    return(s)
