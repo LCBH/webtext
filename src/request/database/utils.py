f224a482a7a758f6bb4ce00682f67fc00761d6a6
Lucca Hirschi <lucca.hirschi@gmail.com>
2015-05-13 17:18:12 +0200

    minor in db/test

--- src/request/database/utils.py
+++ src/request/database/utils.py
@@ -134,9 +134,13 @@ def printInfo():
     for backend in db['backends']:
         logging.info(backend['backend'] + ", API_key: " + backend['API_key'])
     logging.info("## Table Store, size: " + str(len(db['store'])))
-    logging.info("# Columns: " + str(db['store'].columns))
+    logging.info("# Columns: " + ",".join(db['store'].columns))
     for mess in db['store']:
         logging.info(mess['user'] + ", nb: " + str(mess['nb']) + ", message: " + mess['message'])
+    logging.info("## Table yelpIDs, size: " + str(len(db['yelpIDs'])))
+    logging.info("# Columns: " + str(db['yelpIDs'].columns))
+    for busi in db['yelpIDs']:
+        logging.info(busi['user'] + ", nb: " + str(busi['nb']) + ", name: " + busi['name'])
        
 
 def exportJson(tableName='users', filename='toRemove.json'):
