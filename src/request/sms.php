bf7e3f7e7aff0c77fc050f25f8464662c3218765
Lucca Hirschi <lucca.hirschi@gmail.com>
2015-05-27 11:47:36 +0200

    security: protect from shell injection

--- src/request/sms.php
+++ src/request/sms.php
@@ -3,16 +3,16 @@
 // https://www.choum.net/webtext/api/sms.php?pass=API_SECRET_KEYY&numero=NUMERO&content=CONTENT&isTesting=BOOL
 
 $PROGRAM="/home/lutcheti/webtext/src/request/handleSMS.py";
-$PASS = $_GET['pass'];
-$NUMERO = $_GET['numero'];
-$CONTENT = $_GET['content'];
-$isTesting = $_GET['isTesting'];
+$PASS = escapeshellarg($_GET['pass']);
+$NUMERO = escapeshellarg($_GET['numero']);
+$CONTENT = escapeshellarg($_GET['content']);
+$isTesting = escapeshellarg($_GET['isTesting']);
 $isLocal = "false";
 
 echo "<pre>Before</pre>";
 echo "<pre>Input: numero: $NUMERO, content: $CONTENT; pass: $PASS.</pre>";
 // POUR QUITTER LE MODE TESTING et vraiment envoyer les réponses aux requêtes par SMS, mettre le premier boolean à false
-$output = shell_exec('sudo python /home/lutcheti/webtext/src/request/handleSMS.py "'.$NUMERO.'" "'.$CONTENT.'" "'.$isTesting.'" "'.$isLocal.'"  "'.$PASS.'" ');
+$output = shell_exec('sudo python /home/lutcheti/webtext/src/request/handleSMS.py ' . ' ' . $NUMERO . ' ' . $CONTENT . ' ' . $isTesting . ' ' . $isLocal . ' ' . $PASS);
 echo "<pre>Output: (no output but see the logs) $output</pre>";
 echo "<pre>After</pre>";
 ?>
